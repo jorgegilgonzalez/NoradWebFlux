93342ad04a9d694d6b23a2eae129222da3d46604
jorge.gil-gonzalez <jorge.gil-gonzalez@getronics.com>
2023-05-15 00:49:49 +0200

    control de errores

--- radar/src/main/java/com/norad/radar/repository/MisilRepository.java
+++ radar/src/main/java/com/norad/radar/repository/MisilRepository.java
@@ -1,12 +1,19 @@
 package com.norad.radar.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.norad.radar.dto.Misil;
 
+import reactor.core.publisher.Mono;
+
 @Repository
 public interface MisilRepository extends ReactiveCrudRepository<Misil, Integer>{
 
+	//necesito un metodo fuera del CrudRepository para encontrar un misil por nombre para checkear el error en caso de que coincidan dos nombres, por la restriccion unique del nombre en la bd, se define el metodo sin implementarlo por ser interfaz
+	Mono<Misil> findByModelo(String modelo);
 	
+	@Query("SELECT * FROM misiles WHERE id<> :id AND modelo = :modelo")
+	Mono<Misil> findModeloRepetido(int id, String modelo);
 }
--- radar/src/main/java/com/norad/radar/service/MisilService.java
+++ radar/src/main/java/com/norad/radar/service/MisilService.java
@@ -24,21 +24,46 @@ public class MisilService {
 	
 	//metodo que devuelve un mono con el misil encontrado por id
 	public Mono<Misil> getById(int id){
-		return misilRepository.findById(id);
+		
+		
+		return misilRepository.findById(id)
+				//control de errores, como es un mono, aunque no devuelva nada no salta error, generamos uno
+				.switchIfEmpty(Mono.error(new Exception("Ese tipo de misil no existe")));
 	}
 	
 	//metodo que deveulve el misil guardado 
 	public Mono<Misil> save(Misil misil){
-		return misilRepository.save(misil);
+		//control de errores, comprueba si existe el modelo de misil ya en la bd
+		Mono<Boolean> existeModelo = misilRepository.findByModelo(misil.getModelo()).hasElement();
+		//devuelve por ternario un error si existe y si no guarda el misil
+		return existeModelo.flatMap(e-> e ? Mono.error(new Exception("El modelo de misil ya existe en la BD")) : misilRepository.save(misil));
 		
 	}
 	 //metodo que actualiza un misil dando el id y el misil(los nuevos datos)
 	public Mono<Misil> update(int id, Misil misil){
-		return misilRepository.save(new Misil(id, misil.getModelo(), misil.getAlcance()));
+		
+				//control de errores, comprueba si existe el id del misil ya en la bd
+				Mono<Boolean> existeId = misilRepository.findById(id).hasElement();
+				//control de errores, comprueba si existe el modelo de misil ya en la bd
+				Mono<Boolean> existeModelo = misilRepository.findModeloRepetido(id, misil.getModelo()).hasElement();
+				
+				
+				//si existe el modelo de misil lanza error, de lo contrario, si el id es correcto actualizar el producto, de lo contrario lanza error de id no existe - if ternario anidado
+				return existeId.flatMap(e-> e?
+						existeModelo.flatMap(m -> m ?  Mono.error(new Exception("El modelo de misil ya existe en la BD"))
+						: misilRepository.save(new Misil(id, misil.getModelo(), misil.getAlcance())))
+						: Mono.error(new Exception("El id no coincide"))); 
+					 
+					 
+		
 	}
 	
 	//metodo que borra un misil por id
 	public Mono<Void> delete(int id){
-		return misilRepository.deleteById(id);
+		
+		//control de errores, comprueba si existe el id del misil ya en la bd
+		Mono<Boolean> existeId = misilRepository.findById(id).hasElement();
+		
+		return existeId.flatMap(e->e ? misilRepository.deleteById(id) : Mono.error(new Exception("no se puede borrar, misil no encontrado")));
 	}
 }
