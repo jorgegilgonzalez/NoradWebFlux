1765d91469228277b7563d5a4b69c4dfeb3a6879
jorge.gil-gonzalez <jorge.gil-gonzalez@getronics.com>
2023-05-24 08:22:49 +0200

    Especio aereo nucleo

---
+++ espacioaereo/src/main/java/com/rusia/espacioaereo/EspacioaereoApplication.java
@@ -0,0 +1,13 @@
+package com.rusia.espacioaereo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacioaereoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacioaereoApplication.class, args);
+	}
+
+}
---
+++ espacioaereo/src/test/java/com/rusia/espacioaereo/EspacioaereoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusia.espacioaereo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacioaereoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
